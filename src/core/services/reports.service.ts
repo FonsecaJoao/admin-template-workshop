c5af55d3f9de953292e7e614291e3da4446913cf
Joao Fonseca <jmgf_13@hotmail.com>
2022-09-04 06:13:54 +0100

    Implement report service

--- src/core/services/reports.service.ts
+++ src/core/services/reports.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ReportPayload } from '../interfaces/report-payload';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReportsService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  sendReport(payload: ReportPayload): Observable<Object> {
+    return this.httpClient.post(`${environment.apiUrl}report`, payload);
+  }
 }
