bc04d57e3b343884fd4e65c071876261f05889fa
Joao Fonseca <jmgf_13@hotmail.com>
2022-09-04 14:16:20 +0100

    Implement projects dropdown & headings

--- src/app/pages/reports/reports-header/reports-header.component.ts
+++ src/app/pages/reports/reports-header/reports-header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ApiResponse } from 'src/core/interfaces/api-response';
+import { Project } from 'src/core/interfaces/project';
+import { ProjectsService } from 'src/core/services/projects.service';
 
 @Component({
   selector: 'app-reports-header',
   templateUrl: './reports-header.component.html',
-  styleUrls: ['./reports-header.component.scss']
+  styleUrls: ['./reports-header.component.scss'],
 })
 export class ReportsHeaderComponent implements OnInit {
+  protected projectList: Project[] = [];
+  protected filtersForm: FormGroup = new FormGroup({});
+  @Input() navOpened = false;
 
-  constructor() { }
+  constructor(private projectsService: ProjectsService) {}
 
   ngOnInit(): void {
-  }
+    this.filtersForm = new FormGroup({
+      project: new FormControl('AllProjects'),
+    });
 
+    this.projectsService.getProjects().subscribe(
+      (projects) => {
+        const projectsData = (projects as ApiResponse<Project[]>)?.data;
+        if (projectsData) this.projectList = projectsData;
+      },
+      (err) => (this.projectList = [])
+    );
+  }
 }
