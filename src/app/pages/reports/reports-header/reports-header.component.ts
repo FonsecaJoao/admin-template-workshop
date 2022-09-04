48f2f59bbb13dfb7cf9e33628b26a4c62cc2ed0f
Joao Fonseca <jmgf_13@hotmail.com>
2022-09-04 15:33:15 +0100

    Implement gateways dropdown

--- src/app/pages/reports/reports-header/reports-header.component.ts
+++ src/app/pages/reports/reports-header/reports-header.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ApiResponse } from 'src/core/interfaces/api-response';
+import { Gateway } from 'src/core/interfaces/gateway';
 import { Project } from 'src/core/interfaces/project';
+import { GatewaysService } from 'src/core/services/gateways.service';
 import { ProjectsService } from 'src/core/services/projects.service';
 
 @Component({
@@ -11,14 +13,20 @@ import { ProjectsService } from 'src/core/services/projects.service';
 })
 export class ReportsHeaderComponent implements OnInit {
   protected projectList: Project[] = [];
+  protected gatewayList: Gateway[] = [];
   protected filtersForm: FormGroup = new FormGroup({});
+
   @Input() navOpened = false;
 
-  constructor(private projectsService: ProjectsService) {}
+  constructor(
+    private projectsService: ProjectsService,
+    private gatewaysService: GatewaysService
+  ) {}
 
   ngOnInit(): void {
     this.filtersForm = new FormGroup({
       project: new FormControl('AllProjects'),
+      gateway: new FormControl('AllGateways'),
     });
 
     this.projectsService.getProjects().subscribe(
@@ -27,6 +35,14 @@ export class ReportsHeaderComponent implements OnInit {
         if (projectsData) this.projectList = projectsData;
       },
       (err) => (this.projectList = [])
+    );
+
+    this.gatewaysService.getGateways().subscribe(
+      (gateways) => {
+        const gatewaysData = (gateways as ApiResponse<Gateway[]>)?.data;
+        if (gatewaysData) this.gatewayList = gatewaysData;
+      },
+      (err) => (this.gatewayList = [])
     );
   }
 }
