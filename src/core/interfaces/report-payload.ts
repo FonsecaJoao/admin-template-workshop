9b4978f4e3eb5a3566bfa33ec1a10ec1e4cf5111
Joao Fonseca <jmgf_13@hotmail.com>
2022-09-04 06:12:38 +0100

    Create report interfaces

---
+++ src/core/interfaces/report-payload.ts
@@ -0,0 +1,4 @@
+export interface ReportPayload {
+  gatewayId?: string;
+  projectId?: string;
+}
---
+++ src/core/interfaces/report.ts
@@ -0,0 +1,9 @@
+export interface Report {
+  amount: number;
+  created: string;
+  gatewayId: string;
+  modified: string;
+  paymentId: string;
+  projectId: string;
+  userIds: string[];
+}
