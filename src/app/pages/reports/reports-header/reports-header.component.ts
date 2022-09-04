a1c49790c504558797e96b4f056444bce4b23aa2
Joao Fonseca <jmgf_13@hotmail.com>
2022-09-04 18:32:44 +0100

    Implement report header

--- src/app/pages/reports/reports-header/reports-header.component.ts
+++ src/app/pages/reports/reports-header/reports-header.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ApiResponse } from 'src/core/interfaces/api-response';
 import { Gateway } from 'src/core/interfaces/gateway';
 import { Project } from 'src/core/interfaces/project';
@@ -20,15 +22,44 @@ export class ReportsHeaderComponent implements OnInit {
 
   constructor(
     private projectsService: ProjectsService,
-    private gatewaysService: GatewaysService
-  ) {}
+    private gatewaysService: GatewaysService,
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {
+    this.iconRegistry.addSvgIcon(
+      'datepicker',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/date.svg')
+    );
+  }
 
   ngOnInit(): void {
+    this.initialize();
+  }
+
+  protected clearDate(event: Event, key: string): void {
+    event.stopPropagation();
+    this.filtersForm.get(key)?.setValue(null);
+  }
+
+  protected submitForm(): void {
+    console.log(this.filtersForm);
+  }
+
+  private initialize() {
+    this.initForm();
+    this.requestData();
+  }
+
+  private initForm() {
     this.filtersForm = new FormGroup({
       project: new FormControl('AllProjects'),
       gateway: new FormControl('AllGateways'),
+      fromDate: new FormControl({ value: '', disabled: true }),
+      toDate: new FormControl({ value: '', disabled: true }),
     });
+  }
 
+  private requestData() {
     this.projectsService.getProjects().subscribe(
       (projects) => {
         const projectsData = (projects as ApiResponse<Project[]>)?.data;
