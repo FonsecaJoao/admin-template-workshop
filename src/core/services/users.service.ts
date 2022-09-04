f78a9cf7102ddb9a87c831f590dee3f65012c676
Joao Fonseca <jmgf_13@hotmail.com>
2022-09-04 05:49:54 +0100

    Implement users service

--- src/core/services/users.service.ts
+++ src/core/services/users.service.ts
@@ -1,8 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UsersService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  getUsers(): Observable<Object> {
+    return this.httpClient.get(`${environment.apiUrl}users`);
+  }
 }
