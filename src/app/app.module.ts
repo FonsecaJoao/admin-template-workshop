461d251f67b2aee1dd4c720136ecd36a7f374320
Joao Fonseca <jmgf_13@hotmail.com>
2022-09-04 14:14:50 +0100

    Import required modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { ReportsHeaderComponent } from './pages/reports/reports-header/reports-header.component';
 
 @NgModule({
@@ -23,8 +28,11 @@ import { ReportsHeaderComponent } from './pages/reports/reports-header/reports-h
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     MatSidenavModule,
     MatListModule,
+    MatSelectModule,
+    MatFormFieldModule,
     BrowserAnimationsModule,
     HttpClientModule,
   ],
