1a00db424681da36c7806baa844c835af85df05c
Joao Fonseca <jmgf_13@hotmail.com>
2022-09-05 02:40:16 +0100

    Implement the reports table

--- src/app/pages/reports/reports-table/reports-table.component.ts
+++ src/app/pages/reports/reports-table/reports-table.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { ReportFormattedData } from 'src/core/interfaces/report-formatted-data';
+import { ReportProjectData } from 'src/core/interfaces/report-project-data';
 @Component({
   selector: 'app-reports-table',
   templateUrl: './reports-table.component.html',
-  styleUrls: ['./reports-table.component.scss']
+  styleUrls: ['./reports-table.component.scss'],
 })
 export class ReportsTableComponent implements OnInit {
+  protected panelOpenState = false;
+  protected displayedColumns: string[] = [
+    'created',
+    'gatewayName',
+    'paymentId',
+    'amount',
+  ];
 
-  constructor() { }
+  @Input() navOpened = false;
+  @Input() reportData: ReportFormattedData = {};
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
