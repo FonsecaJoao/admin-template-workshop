654d311c205aa3de622661884b1d0963b43fdce6
Joao Fonseca <jmgf_13@hotmail.com>
2022-09-05 02:39:22 +0100

    Send post request to reports endpoint & inject formatted data

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { adaptReportDateBasedOnFilterValues } from 'src/app/utils/report.adapter';
+import { ApiResponse } from 'src/core/interfaces/api-response';
+import { Gateway } from 'src/core/interfaces/gateway';
+import { Project } from 'src/core/interfaces/project';
+import { Report } from 'src/core/interfaces/report';
+import { ReportFormattedData } from 'src/core/interfaces/report-formatted-data';
+import { ReportPayload } from 'src/core/interfaces/report-payload';
+import { ReportsService } from 'src/core/services/reports.service';
 
 interface Item {
   icon: string;
@@ -11,6 +19,7 @@ interface Item {
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
+  protected reportData: ReportFormattedData = {};
   protected openSideNav = false;
   protected menuItemList: Item[] = [
     { icon: 'assets/icons/project.svg', name: 'Projects' },
@@ -19,8 +28,9 @@ export class SidenavComponent implements OnInit {
     { icon: 'assets/icons/chart.svg', name: 'Reports' },
     { icon: 'assets/icons/logout.svg', name: 'Logout' },
   ];
+  protected hasResults = false;
 
-  constructor() {}
+  constructor(private reportsService: ReportsService) {}
 
   ngOnInit(): void {}
 
@@ -28,7 +38,35 @@ export class SidenavComponent implements OnInit {
     this.openSideNav = !this.openSideNav;
   }
 
-  requestReport(event: { [key: string]: string }): void {
-    // TODO
+  requestReport(event: {
+    project: string;
+    gateway: string;
+    fromDate: string;
+    toDate: string;
+    projectsData: Project[];
+    gatewaysData: Gateway[];
+  }): void {
+    const payload: ReportPayload = {
+      projectId: event.project === 'AllProjects' ? '' : event.project,
+      gatewayId: event.gateway === 'AllGateways' ? '' : event.gateway,
+      from: event.fromDate,
+      to: event.toDate,
+    };
+    this.reportsService.sendReport(payload).subscribe((data) => {
+      const reportData = (data as ApiResponse<Report[]>).data;
+
+      if (reportData.length > 1) {
+        this.reportData = adaptReportDateBasedOnFilterValues(
+          reportData,
+          payload,
+          event.projectsData,
+          event.gatewaysData
+        );
+
+        this.hasResults = true;
+      } else {
+        this.reportData = {};
+      }
+    });
   }
 }
